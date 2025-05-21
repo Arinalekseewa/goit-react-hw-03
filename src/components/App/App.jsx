4748d0e29b12b3110ca0acbfebf1d020577d1a35
Ariel <arina.kudar@gmail.com>
2025-05-21 15:02:34 +0300

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"; 
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  )
+}
+
+export default App
